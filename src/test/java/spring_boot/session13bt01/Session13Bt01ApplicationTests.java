a38620c1159f2fa6435d1d76967c4c849deaf5e3
bangtrongtu <bangtu2005.com@gmail.com>
2026-04-22 22:42:49 +0700

    bt01

---
+++ src/test/java/spring_boot/session13bt01/Session13Bt01ApplicationTests.java
@@ -0,0 +1,13 @@
+package spring_boot.session13bt01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Session13Bt01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
